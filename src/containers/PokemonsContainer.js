e9e73546cf171e1ffe7fb45614071ebb37f54c19
Harry <hrgus3@gmail.com>
2022-04-15 17:22:35 -0400

    Converts PokemonsContainer.js from Class Component to Functional Component.

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -5,63 +5,115 @@ import { Switch, Route } from "react-router-dom";
 import PokemonForm from "../components/PokemonForm";
 import TeamPokemon from "../components/TeamPokemon";
 
-class PokemonsContainer extends Component {
-  componentDidMount() {
-    console.log("POKECONTAINER MOUNT");
-  }
+function PokemonsContainer(props) {
+  // componentDidMount() {
+  //   console.log("POKECONTAINER MOUNT");
+  // }
+  const pokemons = props.pokemons.map((pokemon) => (
+    <PokemonCard
+      key={pokemon.id}
+      id={pokemon.id}
+      name={pokemon.name}
+      type={pokemon.poke_type}
+      weight={pokemon.weight}
+      frontImage={pokemon.front_image}
+      backImage={pokemon.back_image}
+      addPokemon={props.addPokemon}
+      team={props.team}
+    />
+  ));
+  // Above Functions returns an array of pokemon cards.
 
-  render() {
-    const pokemons = this.props.pokemons.map((pokemon) => (
-      <PokemonCard
-        key={pokemon.id}
-        id={pokemon.id}
-        name={pokemon.name}
-        type={pokemon.poke_type}
-        weight={pokemon.weight}
-        frontImage={pokemon.front_image}
-        backImage={pokemon.back_image}
-        addPokemon={this.props.addPokemon}
-        team={this.props.team}
-      />
-    ));
-    // Above Functions returns an array of pokemon cards.
+  return (
+    <div id="poke-container">
+      <Switch>
+        <Route
+          exact
+          path="/pokemons/new"
+          component={(routeInfo) => {
+            console.log(routeInfo);
 
-    return (
-      <div id="poke-container">
-        <Switch>
-          <Route
-            exact
-            path="/pokemons/new"
-            component={(routeInfo) => {
-              console.log(routeInfo);
+            return (
+              <PokemonForm goBack={() => routeInfo.history.push("/pokemons")} />
+            );
+          }}
+        />
 
-              return (
-                <PokemonForm
-                  goBack={() => routeInfo.history.push("/pokemons")}
-                />
-              );
-            }}
-          />
-
-          <Route exact path="/pokemons">
-            ` <FilterBar />
-            {pokemons}
-          </Route>
-          <Route
-            path="/pokemons/:id"
-            component={(routeInfo) => {
-              console.log(routeInfo);
-              const paramsId = parseInt(routeInfo.match.params.id);
-              const foundPokemon = this.props.pokemons.find(
-                (p) => p.id === paramsId
-              );
-              return <TeamPokemon pokemon={foundPokemon} />;
-            }}
-          />
-        </Switch>
-      </div>
-    );
-  }
+        <Route exact path="/pokemons">
+          ` <FilterBar />
+          {pokemons}
+        </Route>
+        <Route
+          path="/pokemons/:id"
+          component={(routeInfo) => {
+            console.log(routeInfo);
+            const paramsId = parseInt(routeInfo.match.params.id);
+            const foundPokemon = props.pokemons.find((p) => p.id === paramsId);
+            return <TeamPokemon pokemon={foundPokemon} />;
+          }}
+        />
+      </Switch>
+    </div>
+  );
 }
+
+// class PokemonsContainer extends Component {
+//   componentDidMount() {
+//     console.log("POKECONTAINER MOUNT");
+//   }
+
+//   render() {
+//     const pokemons = this.props.pokemons.map((pokemon) => (
+//       <PokemonCard
+//         key={pokemon.id}
+//         id={pokemon.id}
+//         name={pokemon.name}
+//         type={pokemon.poke_type}
+//         weight={pokemon.weight}
+//         frontImage={pokemon.front_image}
+//         backImage={pokemon.back_image}
+//         addPokemon={this.props.addPokemon}
+//         team={this.props.team}
+//       />
+//     ));
+//     // Above Functions returns an array of pokemon cards.
+
+//     return (
+//       <div id="poke-container">
+//         <Switch>
+//           <Route
+//             exact
+//             path="/pokemons/new"
+//             component={(routeInfo) => {
+//               console.log(routeInfo);
+
+//               return (
+//                 <PokemonForm
+//                   goBack={() => routeInfo.history.push("/pokemons")}
+//                 />
+//               );
+//             }}
+//           />
+
+//           <Route exact path="/pokemons">
+//             ` <FilterBar />
+//             {pokemons}
+//           </Route>
+//           <Route
+//             path="/pokemons/:id"
+//             component={(routeInfo) => {
+//               console.log(routeInfo);
+//               const paramsId = parseInt(routeInfo.match.params.id);
+//               const foundPokemon = this.props.pokemons.find(
+//                 (p) => p.id === paramsId
+//               );
+//               return <TeamPokemon pokemon={foundPokemon} />;
+//             }}
+//           />
+//         </Switch>
+//       </div>
+//     );
+//   }
+// }
 
 export default PokemonsContainer;
