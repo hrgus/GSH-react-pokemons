08458b0c2afa55560abf7e5d115fa152221734ba
Harry <hrgus3@gmail.com>
2022-04-15 11:46:07 -0400

    Converts PokemonCard.js from a Class Component to a Functional Component.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,50 +1,42 @@
 import React, { Component } from "react";
 
-class PokemonCard extends Component {
+function PokemonCard(props) {
   // Return true of false if pokemon has been caught
-  caughtPokemon = () => {
-    return !!this.props.team.find((pokemon) => pokemon.id === this.props.id);
+  const caughtPokemon = () => {
+    return !!props.team.find((pokemon) => pokemon.id === props.id);
   };
 
-  componentDidUpdate() {
-    console.log("UPDATED");
-  } // NORMALLY DO NOT WANT TO SET STATE unless oyu have a conditional that will stop
-  // The infinite loop.
+  // Need to convert to use React.memo
 
-  shouldComponentUpdate(nextProps, nextState) {
-    // should update if it hasnt been caught and in the next props it has been caught
-    // this.props.team <- not in here. nextProps.team <- Is in here
-    // If we want to update the last statement must return true
-    // true        &&                  //true               ->                true
-    return (
-      !this.caughtPokemon() &&
-      !!nextProps.team.find((pokemon) => pokemon.id === this.props.id)
-    );
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // should update if it hasn't been caught and in the next props it has been caught
+  //   // this.props.team <- not in here. nextProps.team <- Is in here
+  //   // If we want to update the last statement must return true
+  //   // true        &&                  //true               ->                true
+  //   return (
+  //     !this.caughtPokemon() &&
+  //     !!nextProps.team.find((pokemon) => pokemon.id === this.props.id)
+  //   );
+  // }
 
-  render() {
-    return (
-      <div
-        id={`pokemon-card-${this.props.id}`}
-        className="card"
-        style={{
-          backgroundColor: `var(--${this.props.type})`,
-          backgroundImage: `var(--${
-            this.caughtPokemon() ? "caught" : "unCaught"
-          })`,
-        }}
-      >
-        <h3>{this.props.name}</h3>
-        <p>Type: {this.props.type}</p>
-        <p>Weight: {this.props.weight}</p>
-        <button onClick={(e) => this.props.addPokemon(this.props.id)}>
-          {this.caughtPokemon() ? "Remove From Team" : "Add To Team"}
-        </button>
-        <br />
-        <img alt={this.props.name + " image"} src={this.props.frontImage} />
-      </div>
-    );
-  }
+  return (
+    <div
+      id={`pokemon-card-${props.id}`}
+      className="card"
+      style={{
+        backgroundColor: `var(--${props.type})`,
+        backgroundImage: `var(--${caughtPokemon() ? "caught" : "unCaught"})`,
+      }}
+    >
+      <h3>{props.name}</h3>
+      <p>Type: {props.type}</p>
+      <p>Weight: {props.weight}</p>
+      <button onClick={(e) => props.addPokemon(props.id)}>
+        {caughtPokemon() ? "Remove From Team" : "Add To Team"}
+      </button>
+      <br />
+      <img alt={props.name + " image"} src={props.frontImage} />
+    </div>
+  );
 }
-
 export default PokemonCard;
